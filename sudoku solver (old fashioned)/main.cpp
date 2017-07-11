32c30ba36a2f521e9de417dede44bae0e266659f
thefireholder <aaccjjt@hotmail.com>
2017-07-11 04:35:24 -0700

    Initial Commit

---
+++ sudoku solver (old fashioned)/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  sudoku solver (old fashioned)
+//
+//  Created by Sangjoon Lee on 7/11/17.
+//  Copyright © 2017 Sangjoon Lee. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
